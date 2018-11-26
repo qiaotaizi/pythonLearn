abd6cf0e51a8bbe62cd1794e06cbaccb938a7652
zhiheng.jiang <zhiheng.jiang@ttpai.cn>
2018-11-26 18:09:42 +0800

    继承进阶

--- oop_extend.py
+++ oop_extend.py
@@ -43,9 +43,33 @@ def animal_run(a):
 
 animal_run(animal)
 
+
 # 鸭子类型:
 # 一只鸟,只要它叫起来像鸭子,跑起来像鸭子,游泳时像鸭子,我们就认为它是只鸭子
 # 对于类似python的动态语言,
 # 方法在传参和定义时,编译器(解释器)不会立刻检查参数的类型,
 # 仅关注方法的调用,如果传入一个对象,该对象没有调用的方法,
 # 则会在运行期抛出异常.
+
+# 多继承(MixIn)
+# 例如Tiger继承了Animal类,同时还要继承Runable(可奔跑)和Carnivorous(肉食性)
+# 就可以这样设计
+class RunableMixIn(object):
+    def move(self):
+        print("move as run")
+
+
+class CarnivorousMixIn(object):
+    def eat(self):
+        print("eat meat")
+
+
+class Tiger(Animal, RunableMixIn, CarnivorousMixIn):
+    pass
+
+
+tiger = Tiger()
+
+tiger.run()
+tiger.move()
+tiger.eat()
--- oop_reflect.py
+++ oop_reflect.py
@@ -10,6 +10,7 @@
 __author__ = 'jaiz'
 
 import types
+from types import MethodType
 
 
 # 使用type函数获取类型(函数)信息
@@ -63,3 +64,86 @@ print(getattr(mc, 'att', 'ojbk'))
 # 可以用del obj.attr删除属性
 print(MyClass.static_attr)
 del MyClass.static_attr
+
+
+class MyClass2(object):
+    pass
+
+
+# python允许动态给实例绑定属性和方法
+mc2 = MyClass2()
+# 实例绑定属性
+mc2.name = 'mc2'
+print(mc2.name)
+
+
+# 实例绑定方法
+def set_age(self, age):
+    self.age = age
+
+
+mc2.set_age = MethodType(set_age, mc2)
+
+mc2.set_age(20)
+
+print(mc2.age)
+
+
+# 给一个实例绑定方法,对另外一个实例时无效的
+# 为了给所有实例绑定方法,可以给类绑定方法
+def set_score(self, score):
+    self.score = score
+
+
+MyClass2.set_score = set_score
+
+mc2.set_score(90)
+print(mc2.score)
+
+
+# 使用slots限制实例属性的绑定
+class MyClass3(object):
+    # 定义__slots__之后,对象只能绑定__slots__声明的属性
+    # __slots__不会对继承的子类起作用
+    # 但是如果子类也定义了__slots__
+    # 那么子类的可绑定属性是父类__slots__和自身__slots__的并集
+    __slots__ = ('name', 'age')
+
+
+# 使用装饰器对属性进行限制
+class MyClass4(object):
+
+    def __init__(self):
+        self._score = 0
+
+    # 相当于get_score
+    @property
+    def score(self):
+        return self._score
+
+    # 相当于set_score
+    # 如果不定义该方法的话,score视为只读属性
+    @score.setter
+    def score(self, score):
+        if not isinstance(score, int):
+            raise ValueError("分数必须是整数")
+        if score > 100:
+            raise ValueError("分数不能超过100")
+        self._score = score
+
+    @property
+    def level(self):
+        if self._score<60:
+            return '不及格'
+        elif self._score<90:
+            return '良好'
+        else:
+            return '优秀'
+
+
+mc4 = MyClass4()
+mc4.score = 10
+print(mc4.score)
+print(mc4.level)
+mc4.score = 100
+print(mc4.level)
