4677013056d5a7d1b7e72def0a3f5b68a2106bab
zhiheng.jiang <zhiheng.jiang@ttpai.cn>
2018-11-30 20:12:14 +0800

    面向对象-特殊方法

--- oop.py
+++ oop.py
@@ -64,3 +64,83 @@ lily.print_score()
 lily.set_name('liu')
 lily.set_score(89)
 lily.print_score()
+
+
+# 面向对象的几个特殊方法
+class MyClass(object):
+    def __init__(self, name):
+        self.name = name
+
+    # __str__相当于java toString
+    def __str__(self):
+        return 'Student object (name:%s)' % self.name
+
+    # 类似于__str__,但用于调试的__repr__
+    __repr__ = __str__
+
+    # 定义__iter__方法后对象可以被迭代
+    # 要和__next__方法一起定义
+    # __getitem__方法使得对象获得根据下标获取元素和切片的能力,类似于list
+    # __iter__,__next__和__getitem__的应用参考下方Fib类
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        pass
+
+    def __getitem__(self):
+        pass
+
+
+print(MyClass('Micheal'))
+
+
+class Fib(object):
+    # init方法定义两个初始值
+    def __init__(self):
+        self.a = 0
+        self.b = 1
+
+    def __iter__(self):
+        # 实例本身就是迭代对象,返回自己
+        return self
+
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b
+        # 停止遍历的条件
+        if self.a > 1000:
+            raise StopIteration()
+        return self.a
+
+    # list可能根据索引获取单个元素,也可能根据切片获取一个新的list
+    # 要使得自己Fib类实例获取切片的能力
+    # 需要这样编写getitem
+    def __getitem__(self, n):
+        if isinstance(n, int):
+            a, b = 1, 1
+            for x in range(n):
+                a, b = b, a + b
+            return a
+        if isinstance(n, slice):
+            start = n.start
+            stop = n.stop
+            if start is None:
+                start = 0
+            a, b = 1, 1
+            L = []
+            for x in range(stop):
+                if x >= start:
+                    L.append(a)
+                a, b = b, a + b
+            return L
+
+
+# 构造函数返回可迭代对象,直接用于for循环
+# 每次循环调用__next__函数获取下一个值
+fib = Fib()
+for n in fib:
+    print(n)
+
+print(fib[10])
+
+print(fib[5:10])
